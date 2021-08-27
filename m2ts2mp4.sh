8398527d4c0e901d3a44220347ccf15b55293d4c
rix <rixwoodling@gmail.com>
2021-08-26 20:25:34 -0700

    Update m2ts2mp4.sh

--- m2ts2mp4.sh
+++ m2ts2mp4.sh
@@ -5,8 +5,7 @@
 
 function vcrop {
     echo "detecting video dimensions"
-    ffmpeg -i *.m2ts -t 1 -vf cropdetect -f null - 2>&1 | \
-    awk '/crop/ { print $NF }' | tail -1
+    ffmpeg -i ./m2ts/*.m2ts -t 1 -vf cropdetect -f null - 2>&1 | awk '/crop/ { print $NF }' | tail -1
 }
 
 function main {
@@ -19,16 +18,13 @@ if [ ! -d ./m2ts ]; then mkdir m2ts; fi
 if [ ! -d ./mp4 ]; then mkdir mp4; fi
 
 if ! command -v ffmpeg &> /dev/null; then echo "ffmpeg is not installed"
-elif ! command  -v ffprobe &> /dev/null; then echo "ffprobe is not installed"
+elif ! command -v ffprobe &> /dev/null; then echo "ffprobe is not installed"
 fi
 
-if [[ $( \ls | wc -l ) = 0 ]]; then :
-elif [[ $( \ls | wc -l ) < 1 ]]; then echo "too many files in source folder"
-else if [ ! -f *.m2ts ]; then :; else main; fi
+if [[ $( \ls ./m2ts | wc -l ) -eq 0 ]]; then echo "no m2ts file found"
+elif [[ $( \ls ./m2ts | wc -l ) -gt 1 ]]; then echo "too many files in source folder"
+else if [ ! -f ./m2ts/*.m2ts ]; then echo "file is not an m2ts file"; else main; fi
 fi
 
 ### function calls
-
-
-
 
