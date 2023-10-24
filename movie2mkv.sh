4f1c41c10f3ee114074ce26a2804db493c42bd37
Rix <rixwoodling@gmail.com>
2023-10-23 22:13:00 -0700

    Add numerous comments

--- movie2mkv.sh
+++ movie2mkv.sh
@@ -2,14 +2,15 @@
 # movie2mkv v7
 
 # automatically convert or copy all streams of a movie file into mkv
-
+# tested with ffmpeg 3.2.15-0+deb9u2
 
 ### --- ERROR HANDLING --- ###
 
-# tested with ffmpeg 3.2.15-0+deb9u2
+# check if ffmpeg is installed, if not, exit
 if ! command -v ffmpeg &>/dev/null; then echo "ffmpeg is not installed"; exit 1
 else echo -n "ffmpeg version "; echo $( ffmpeg -version | head -n1 | awk '{print $3}' ); fi
 
+# check if argument is provided, if not exit
 if [[ -z "$1" ]]; then echo "command requires 1 argument"; exit 1
 elif [[ ! -z "$2" ]]; then echo "command requires no more than 1 argument"; exit 1; fi
 
@@ -21,7 +22,7 @@ elif [[ ! -z "$2" ]]; then echo "command requires no more than 1 argument"; exit
 
 ### --- OPTIONS --- ###
 
-# define if deinterlacing is required
+# define if deinterlacing is required, will return 0 if true
 deint=$( ffmpeg -hide_banner -filter:v idet -frames:v 100 -an -f rawvideo -y /dev/null -i "$1" 2>&1 | grep -m 1 BFF | sed 's/.*TFF\:\ *//' | sed 's/[^0-9].*//' )
 if [ ! "$deint" -eq 0 ]; then
     deint=$( echo -n "-vf bwdif=mode=0" )
@@ -42,33 +43,45 @@ options=$( echo -n '-pix_fmt + -vf '$vfcrop $deint '-map_metadata 0 -vsync vfr '
 # VIDEOSTREAMS
 v=0
 vid=()
+# loop once
 while [ ! "$v" -eq 1 ]; do
+    # get video language of movie, set to undetermined if blank
     lang=$( ffprobe -i $1 v:$v -loglevel quiet -show_entries stream_tags=language -select_streams v -of default=noprint_wrappers=1:nokey=1 )
     if [ -z $lang ]; then lang="und"; fi
+    # for video settings, set to libx264, bitrate to 8M, tune to film, set profile to high/4.0, keep metadata, add language
     vid+=$( echo -n '-c:v libx264 -b:v:'$v' 8M -tune film -vprofile high -vlevel 4.0 -metadata:s:v:'$v' language='$lang )
     v=$(( v + 1 ))
 done
 videostream="${vid[@]}"
+# -c:v libx264 -b:v:0 8M -tune film -vprofile high -vlevel 4.0 -metadata:s:v:0 language=eng
 
 # AUDIOSTREAMS
 a=0
 f=1
 aud=()
+# loop through each audio stream
 while true; do
+    # get audio language for each stream
     lang=$( ffprobe -i $1 a:$a -loglevel quiet -show_entries stream_tags=language -select_streams a -of default=noprint_wrappers=1:nokey=1 )
     if [ -z $lang ]; then lang="und"; fi
+    # get audio format for each stream
     audioformat=$( ffmpeg -filter:v idet -frames:v 100 -f rawvideo -y /dev/null -i "$1" 2>&1 | grep "Audio:\ *" | grep "#0:"$f | awk '{print $4}' | sed 's/[^a-zA-Z0-9]//g' )
+    # if audio is detected
     if [[ ! -z $audioformat ]]; then
+        # if audioformat is dts, ac3 or aac, then just copy
         if [[ "$audioformat" == "dts" || "$audioformat" == "ac3" || "$audioformat" == "aac" ]]; then
             aud+=$( echo -n '-c:a:'$a' copy ' )
+        # check number of channels in stream, set to convert all channels to aac    
         else
             audiostreams=$( ffprobe -select_streams a:$a -v error -show_entries program_stream=channels -of default=noprint_wrappers=1:nokey=1 "$1" )
-            if [[ $audiostreams -eq 6 ]]; then
-                aud+=$( echo -n '-ac 6 aac -b:a 384k -ar 48000 -metadata:s:a:'$a' language='$lang' ' )
-            else
-                aud+=$( echo -n '-ac 2 aac -b:a 384k -ar 48000 -metadata:s:a:'$a' language='$lang' ' )
-            fi
+            #aud+=$( echo -n '-ac '$audiostreams' aac -b:a 384k -ar 48000 -metadata:s:a:'$a' language='$lang' ' ) 
+            if [[ $audiostreams -eq 6 ]]; then #
+                aud+=$( echo -n '-ac 6 aac -b:a 384k -ar 48000 -metadata:s:a:'$a' language='$lang' ' ) #
+            else #
+                aud+=$( echo -n '-ac 2 aac -b:a 384k -ar 48000 -metadata:s:a:'$a' language='$lang' ' ) #
+            fi #
         fi
+    # if no audio detected, break loop
     else
         break
     fi
@@ -96,14 +109,17 @@ subtitlestreams="${sub[@]}"
 
 ### --- STDOUT --- ###
 
-echo "ffmpeg -i $1"
+echo "ffmpeg -probesize 100M -analyzeduration 100M"
+echo "-i $1"
 echo "$options"
 echo "-map 0"
 if [ ! -z "${vid[@]}" ]; then echo "${vid[@]}"; fi
+# sed expression creates newlines at each -c
 if [ ! -z "${aud[@]}" ]; then echo "${aud[@]}" | sed 's/-c/\n-c/2g'; fi
 if [ ! -z "${sub[@]}" ]; then echo "${sub[@]}" | sed 's/-c/\n-c/2g'; fi
 echo "$1.mkv"; echo
 
+# auto map all streams
 maps="-map 0"
 
 ffmpeg -probesize 100M -analyzeduration 100M -i $1 $options -loglevel warning -stats $maps $videostream $audiostreams $subtitlestreams $1.mkv
